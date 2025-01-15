0995320643671d9336755dbd3735d2c4cd5b1258
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-01-15 19:03:06 +0000

    feat: added auth

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,45 @@
 import AuthForm from '../components/AuthForm';
+import {json, redirect} from 'react-router-dom'
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({request}){
+
+  const searchParams = new URL(request.url).searchParams
+  const mode = searchParams.get('mode') || 'login'
+
+
+  if(mode !== 'login' && mode !== 'signup' ){
+    throw json({message: 'Unsoported mode '}, {status: '422'})
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password')
+  };
+
+  const response = await fetch('https://shiny-memory-95vxqg6vg6vhrjq-8080.app.github.dev/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify(authData)
+  } )
+
+
+  if(response.status === 422 || response.status === 401){
+      return response; 
+    }
+
+   if(!response.ok){
+    throw json({message: 'coudl not authenticate user  '}, {status: '500'})
+   } 
+
+   // manage token 
+   return redirect('/')
+}
